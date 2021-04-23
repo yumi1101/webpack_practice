83f79653b8108b48439c8ff7247a5a287d425a09
yumi1101 <81450724+yumi1101@users.noreply.github.com>
2021-04-24 08:22:41 +0900

    Generate HTML through Pug

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,20 @@ module.exports = {
                    },
                 ],
             },
+            {
+                test: /\.pug/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                    {
+                        loader: 'pug-html-loader',
+                        options: {
+                            pretty: true,
+                        }
+                    },
+                ],
+            },
         ],
     },
     plugins: [
@@ -42,7 +56,12 @@ module.exports = {
             filename: './stylesheets/main.css',
         }),
         new HtmlWebpackPlugin({
-            template: './src/templates/index.html',
+            template: './src/templates/index.pug',
+            filename: 'index.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/access.pug',
+            filename: 'access.html',
         }),
         new CleanWebpackPlugin(),
     ]
